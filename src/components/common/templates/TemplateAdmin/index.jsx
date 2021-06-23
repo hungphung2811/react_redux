bd61cb9b21d5f7758072c9d0bba6fbf71e268bb0
hungphung2811 <70595130+hungphung2811@users.noreply.github.com>
2021-06-24 04:10:16 +0700

    update

--- src/components/common/templates/TemplateAdmin/index.jsx
+++ src/components/common/templates/TemplateAdmin/index.jsx
@@ -3,6 +3,25 @@ import SideBarAdmin from 'components/admin/organisms/SideBarAdmin';
 import React from 'react';
 
 function TemplateAdmin({ children }) {
+    // const history = useHistory()
+    // const { token, user } = getFromLocalStorage('user');
+    // const checkAdmin = checkAdminLocal(user, token);
+    // console.log(checkAdmin);
+    // if (!checkAdmin) {
+    //     console.log('loi local');
+    //     history.push('/home')
+    //     window.alert('da co loi xay ra . vui long thu lai')
+    // }
+    // (async () => {
+    //     try {
+    //         const dataUserAdmin = await AuthApi.checkAdmin(user._id, token);
+    //         console.log(dataUserAdmin);
+    //     } catch (error) {
+    //         history.push('/home')
+    //         window.alert('da co loi xay ra . vui long thu lai')
+    //     }
+    // })();
+
     return (
         <>
             <div className="grid grid-cols-12 container mx-auto">
